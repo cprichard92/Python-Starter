1784db1330a644608c6081d0064cbfcc328ab9d5
Richardson <christopher.richardson@capitalone.com>
2016-12-20 17:01:19 -0500

    pushing examples

--- Hello/src/Hello.py
+++ Hello/src/Hello.py
@@ -15,39 +15,45 @@ float = float(7.23)
 print float
 
 
-#Apostrophes Usage
-bonjour="hello's"
-hello ="hello"
+# Apostrophes Usage
+bonjour = "hello's"
+hello = "hello"
 print bonjour
 
-three=3
-one=1
+three = 3
+one = 1
 solution = three + one + float
 print solution
 
-#Array List Testing
-array =[]
+# Array List Testing
+array = []
 array.append(4)
 array.append(5)
 array.append(6)
 for x in array : print x
 
-#Printing an Array Index
-arraytest = 4,5,6,3,3
+# Printing an Array Index
+arraytest = 4, 5, 6, 3, 3
 print arraytest[1]
 
 
-#If statements with strings (use format function to print placeholders in string
+# If statements with strings (use format function to print placeholders in string
 if bonjour == "hello's" : print "Bonjour: %s" % bonjour
 if bonjour == "hello" : print "Bonjour: {0}".format(hello)
 
-print "The sum of 1 + 2 is {0} ".format(1+2)*2
+print "The sum of 1 + 2 is {0} ".format(1 + 2) * 2
 
 
-#Arithmatic
+# Arithmatic
 number = 1 + 2 * 3 / 4.0
 print(number)
 
-#Exponents
-answer = 7**2
+# Exponents
+answer = 7 ** 2
 print answer
+
+
+# % Operator & format functions
+name='John'
+print "Hello, %s" % name
+print "Hello, {0}".format(name)
